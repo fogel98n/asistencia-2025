3d6e1c9447340adf20f42967f8e8859dec5a9802
fogel98n <fogel98nex@gmail.com>
2025-07-07 19:48:25 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 
-export const BASE_URL = "http://localhost:3000";
-//export const BASE_URL = "https://asistencia.proyectoscl.space";
+//export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://asistencia.proyectoscl.space";
